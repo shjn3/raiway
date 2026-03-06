7563939313132186bd01cece2f92dd71ef938f45
shine <clangod002@gmail.com>
2026-03-06 10:03:01 +0700

    commit

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,5 +4,10 @@ import userRoutes from "./user.routes.js";
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.get('/', (
+    req, res
+) => {
+    res.send("hello world");
+})
 export default router;
 //# sourceMappingURL=index.js.map
